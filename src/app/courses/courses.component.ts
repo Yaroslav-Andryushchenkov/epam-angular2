81087391648a0cb03e72618610a1988e9b79982c
Yaroslav-Andryushchenkov <7098013@gmail.com>
2017-03-14 02:15:52 +0300

    add course data

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
+import { CourseDetailesInterface } from './';
 
 @Component({
   selector: 'courses',
@@ -10,6 +12,34 @@ import {
     <course-detailes></course-detailes>
   `
 })
-export class CoursesComponent  {
+export class CoursesComponent implements OnInit {
+  public courses: [CourseDetailesInterface];
+  constructor() {
+    this.courses = [];
+  }
 
+  public ngOnInit() {
+    this.courses = [{
+      id: 1,
+      name: 'First course',
+      description: 'Description of first course',
+      duration: 11,
+      publishDate: new Date()
+      },
+      {
+        id: 2,
+        name: 'Second course',
+        description: 'Description of second course',
+        duration: 12,
+        publishDate: new Date()
+      },
+      {
+        id: 3,
+        name: 'Third course',
+        description: 'Description of third course',
+        duration: 17,
+        publishDate: new Date()
+      },
+    ];
+  }
 }
--- src/app/courses/index.ts
+++ src/app/courses/index.ts
@@ -1,1 +1,2 @@
 export * from './courses.component';
+export * from './courseDetailes';
